f2dd889577f002ee3c2b35d5e7dfc7dd70675b39
sarinah <sarinahhaque@gmail.com>
2025-01-13 19:45:56 +1100

    first commit

---
+++ Print.py
@@ -0,0 +1,8 @@
+#printing a greeting message 
+print ("hello world")
+#printing my name 
+print ("sarinah")
+#Printing a fun fact about myself 
+print ("I enjoy playing video games")
+#performing the simple calculation
+print ("results of 3+5 is",3+5)
